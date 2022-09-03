5f2bd7986514fbc812b106959f8151ff70c6e8e1
manta-3-3 <yakaiser@gmx.ch>
2022-09-03 13:32:05 +0200

    add GET /join/:game_id route

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -10,3 +10,18 @@ exports.game_list = function (req, res, next) {
       res.render("game_list", { title: "Select Game", list_games: data });
     });
 };
+
+// display login for specified game
+exports.get_join_game = function (req, res, next) {
+  res.render("join_game", { title: "Login Game", game_id: req.params.game_id });
+};
+
+// handle login for specified game
+exports.post_join_game = function (req, res, next) {
+  // check first if such game exists
+
+  // reset session all session data is lost -> logged out from all games
+  req.session.regenerate(function (err) {
+    if (err) return next(err);
+  });
+};
--- routes/join.js
+++ routes/join.js
@@ -4,7 +4,13 @@ const router = express.Router();
 // require controller modules
 const joinController = require("../controllers/joinController");
 
-// GET join home page
+// GET request for list of all Game items
 router.get("/", joinController.game_list);
+
+// GET request to log-on for specified game
+router.get("/:game_id", joinController.get_join_game);
+
+// POST request to log-on for specified game
+router.post("/:game_id", joinController.post_join_game);
 
 module.exports = router;
