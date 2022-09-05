4d347b98a71a8631d439fb3b8a396723337be817
manta-3-3 <yakaiser@gmx.ch>
2022-09-05 15:01:54 +0200

    add POST /join/:game_id route

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,5 +1,17 @@
+const { body, validationResult } = require("express-validator");
+const ObjectId = require("mongoose").Types.ObjectId;
+
 // require database models
 const Game = require("../models/game");
+
+// helper functions
+function isValidObjectId(id) {
+  if (ObjectId.isValid(id)) {
+    if (String(new ObjectId(id)) === id) return true;
+    return false;
+  }
+  return false;
+}
 
 // display list of available games
 exports.game_list = function (req, res, next) {
@@ -12,16 +24,74 @@ exports.game_list = function (req, res, next) {
 };
 
 // display login for specified game
-exports.get_join_game = function (req, res, next) {
-  res.render("join_game", { title: "Login Game", game_id: req.params.game_id });
+exports.get_join_game = function (req, res) {
+  res.render("join_game", {
+    title: "Login Game",
+    game_id: req.params.game_id,
+  });
 };
 
 // handle login for specified game
-exports.post_join_game = function (req, res, next) {
-  // check first if such game exists
+exports.post_join_game = [
+  // validate and sanitize fields
+  body("playerName")
+    .trim()
+    .escape()
+    .isLength({ max: 40 })
+    .withMessage("Player Name cannot be longer than 40 characters.")
+    .isAlphanumeric()
+    .withMessage("Player Name has non-alphanumeric characters."),
+  // body("playerColor") NOT IMPLEMENTED YET
+  body("gamePassword")
+    .trim()
+    .escape()
+    .custom(function (value, { req }) {
+      return new Promise((resolve, reject) => {
+        // check first if its a valid ObjectId
+        if (!isValidObjectId(req.params.game_id)) {
+          return reject("Invalid ObjectId, No such game exists!");
+        }
+        // fetch game for this game_id from database
+        Game.findById(req.params.game_id)
+          .then((db_game) => {
+            if (db_game == null) {
+              return reject("Sorry, No such game exists at the moment!");
+            }
+            if (db_game.password !== value) {
+              return reject("Sorry, Wrong password for this game!");
+            }
+            return resolve();
+          })
+          .catch((err) => {
+            return reject(err);
+          });
+      });
+    }),
+  // NOT IMPLEMENTED YET: ckeck if maxPlayers in game is not exceeded
 
-  // reset session all session data is lost -> logged out from all games
-  req.session.regenerate(function (err) {
-    if (err) return next(err);
-  });
-};
+  function (req, res) {
+    // extract the validation errors from a request
+    const valErrors = validationResult(req);
+    if (!valErrors.isEmpty()) {
+      // there are errors. Render form again with sanitized values/errors messages
+      return res.render("join_game", {
+        title: "Login Game",
+        game_id: req.params.game_id,
+        valErrors: valErrors.array(),
+      });
+    }
+    // data from form is valid -> continue
+
+    // reset session all session data is lost -> logged out from all games
+    req.session.regenerate(function (err) {
+      if (err) return next(err);
+      // assigne data to session
+      req.session.game_id = req.params.game_id;
+      req.session.playerName = req.body.playerName;
+      req.session.playerColor = "none"; // later req.body.playerColor
+      req.session.playerAnswer = null;
+      // redirect to the play route of this game
+      res.redirect(`/play/${req.params.game_id}`);
+    });
+  },
+];
