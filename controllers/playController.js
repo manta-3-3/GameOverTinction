4fcc6bffc295e1d4164fa49f3bcb2e1d724de572
manta-3-3 <yakaiser@gmx.ch>
2022-09-10 18:21:46 +0200

    implement route to display specified game info

--- controllers/playController.js
+++ controllers/playController.js
@@ -1,19 +1,76 @@
 // require database models
 const Game = require("../models/game");
+const Session = require("../models/session");
 
-exports.get_play_index = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: GET /play");
+// mapping gameStatus from database to corresponding route and vice versa
+const routeToDb = {
+  answer: "collectingAnswers",
+  vote: "voting",
+  results: "showVotingResults",
+};
+const dbToRoute = {
+  collectingAnswers: "answer",
+  voting: "vote",
+  showVotingResults: "results",
 };
 
-exports.get_play_game = function (req, res, next) {
-  // check if this user got access to this game via session-cookie
+// middleware to check if this user got access to this game via session-cookie and fetch game-info form database
+exports.authForGame_id = function (req, res, next) {
+  // check access by session-cookie
   if (req.session.game_id !== req.params.game_id) {
     return res.redirect(`/join/${req.params.game_id}`);
   }
-  // access permitted go on
-  res.send("NOT IMPLEMENTED: You got access to this game, go ahed");
+  //TODO: callback hell, fix it with async parallel
+  // fetch game for this game_id from database
+  Game.findById(req.params.game_id)
+    .select("-password")
+    .exec(function (err, db_game) {
+      if (err) return next(err);
+      if (db_game == null) {
+        // no results
+        const err = new Error("Game not found");
+        err.status = 404;
+        return next(err);
+      }
+      // assign db_game to locals
+      res.locals.db_game = db_game;
+      // fetch other game infos from sessions and assign to locals
+      Session.countTotalPlayersByGame_id(db_game.id).exec(function (
+        err,
+        count
+      ) {
+        if (err) return next(err);
+        res.locals.sess_game = { totalPlayers: count };
+        // assign session data to locals
+        res.locals.sess_user = req.session;
+        // access permitted go on
+        next();
+      });
+    });
 };
 
-exports.post_play_game = function (req, res, next) {
-  res.send("NOT IMPLEMENTED: POST /play/:game_id route");
+// middleware to controle the current gameStatus route flow
+exports.controlGameStatus = function (req, res, next) {
+  if (res.locals.db_game.gameStatus === routeToDb[req.params.gameStatus]) {
+    next();
+  } else {
+    res.redirect(
+      `/play/${req.params.game_id}/${dbToRoute[res.locals.db_game.gameStatus]}`
+    );
+  }
+};
+
+// TODO:
+exports.get_play_index = function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+};
+
+exports.get_play_game_info = function (req, res, next) {
+  const continueURL = `/play/${req.params.game_id}/${
+    dbToRoute[res.locals.db_game.gameStatus]
+  }`;
+  res.render("play_game_info", {
+    title: "Welcome to the game!",
+    continueURL: continueURL,
+  });
 };
--- routes/play.js
+++ routes/play.js
@@ -4,13 +4,46 @@ const router = express.Router();
 // require controller modules
 const playController = require("../controllers/playController");
 
+// auth for game_id middleware
+router.use("/:game_id", playController.authForGame_id);
+
+// control current gameStatus middleware
+router.use("/:game_id/:gameStatus", playController.controlGameStatus);
+
 // GET request for index
 router.get("/", playController.get_play_index);
 
-// GET request for playing specified game
-router.get("/:game_id", playController.get_play_game);
+// GET request for playing specified game, welcome/info page for this game
+router.get("/:game_id", playController.get_play_game_info);
 
-// POST request for playing specified game
-router.post("/:game_id", playController.post_play_game);
+// TODO: GET request for playing specified game during collectingAnswers phase
+router.get("/:game_id/answer", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
+
+// TODO: POST request for playing specified game during collectingAnswers phase
+router.post("/:game_id/answer", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
+
+// TODO: GET request for playing specified game during voting phase
+router.get("/:game_id/vote", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
+
+// TODO: POST request for playing specified game during voting phase
+router.post("/:game_id/vote", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
+
+// TODO: GET request for playing specified game during showVotingResults phase
+router.get("/:game_id/results", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
+
+// TODO: POST request for playing specified game during showVotingResults phase
+router.post("/:game_id/results", function (req, res, next) {
+  res.send(`NOT IMPLEMENTED: ${req.method} ${req.path} route`);
+});
 
 module.exports = router;
