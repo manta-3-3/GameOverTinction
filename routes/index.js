9c1567c3b591bf383044a146862e3065d13ea694
manta-3-3 <yakaiser@gmx.ch>
2022-09-16 14:46:46 +0200

    restructuring of homepage index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "GameOverTinction" });
+  res.render("index", { title: "Welcome" });
 });
 
 module.exports = router;
